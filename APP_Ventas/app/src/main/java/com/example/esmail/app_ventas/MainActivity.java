578e296895b39ad635266ef2535214b857aa65d2
Desarrollo5 <jantonio.ordonez@kaam.es>
2018-05-16 18:37:46 +0200

    fragments
    he creado fragments

--- APP_Ventas/app/src/main/java/com/example/esmail/app_ventas/MainActivity.java
+++ APP_Ventas/app/src/main/java/com/example/esmail/app_ventas/MainActivity.java
@@ -1,13 +1,108 @@
 package com.example.esmail.app_ventas;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar appbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+    private Fragment fragment = null;
+
+    private FragmentManager mFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragmentManager = getFragmentManager();
+
+        appbar = (Toolbar) findViewById(R.id.appbar);
+        setSupportActionBar(appbar);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_nav_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navView = (NavigationView) findViewById(R.id.navview);
+        //cabecera
+        View hView = navView.getHeaderView(0);
+        //barra navegacion
+        navView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                        boolean fragmentTransaction = false;
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.menu_customers_import:
+                                
+                                break;
+                            case R.id.menus_products_import:
+
+                                break;
+                            case R.id.menu_make_sale:
+
+                                break;
+                            case R.id.menu_export:
+
+                                break;
+                        }
+                        //iniciar fragments
+                        if (fragmentTransaction) {
+                            mFragmentManager.beginTransaction()
+                                    .replace(R.id.content_frame, fragment)
+                                    .addToBackStack(null)
+                                    .commit();
+
+                            menuItem.setChecked(true);
+                            getSupportActionBar().setTitle(menuItem.getTitle());
+                        }
+
+                        drawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+
+    /**
+     * Menu opciones
+     * @param menu
+     * @return
+     */
+       @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
